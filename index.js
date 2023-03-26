6e1e3343192029bd8a9847c111a9b8f0c5e5a0b4
MSchuur <mschuur31@gmail.com>
2023-03-26 09:02:58 -0300

    Rendered the circle and triange but had to add js files for eact to make it work.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Creating the modules required
 const inquirer = require('inquirer');
+const generateSVGLogo = require('./lib/generateSVGLogo');
 const fs = require('fs');
 
 inquirer
@@ -7,7 +8,7 @@ inquirer
     {
       type: 'input',
       message: 'Enter upto three characters to be shown on your logo: ',
-      name:   'charcaters',
+      name:   'chars',
       validate: (input) => {if(input) {return true} else {return 'I require an input'}}
     },
     {
@@ -17,25 +18,28 @@ inquirer
       validate: (input) => {if(input) {return true} else {return 'I require an input'}}
     },
     {
-        type: 'list',
-        message: 'Choose the shape you wish to use for your logo: ',
-        name: 'shapeType',
-        choices: ['Circle', 'Triangle', 'Square']
-    },
-    {
       type: 'input',
       message: 'What color would you like the shape of your logo to be. Enter color keyword or hexidecinal number: ',
       name: 'shapeColor',
       validate: (input) => {if(input) {return true} else {return 'I require an input'}}
     },
+    {
+      type: 'list',
+      message: 'Choose the shape you wish to use for your logo: ',
+      name: 'shapeType',
+      choices: ['Circle', 'Triangle', 'Square']
+  },
 ])
-.then((response) =>
-    console.log(response)      
-  );
+    .then((data) =>
+    fs.writeFile('./examples/logo.svg', generateSVGLogo(data), (err) => {
+      if(err){
+          console.log(err);
+      }
+      else{
+          console.log('Generated logo.svg')
 
-// const init = () => {
-//     return inquirer.prompt(shapeQuestions)
-//     .then((data) => {
-//         console.log(data)
-//     })
-// }
+      }
+    }))
+
+
+
